ed010641ef6bf3d0fd1232e27f806faa1e6389f2
Kaan Irmak <kaanrmak7@gmail.com>
2024-05-04 17:56:41 +0300

    Update enemy.cs

--- darkmucks/Assets/Scripts/enemy.cs
+++ darkmucks/Assets/Scripts/enemy.cs
@@ -56,41 +56,35 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                agent.isStopped = true;
+                if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+                {
+                    Vector3 point;
+                    if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+                    {
+                        Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
+                        agent.SetDestination(point);
+                    }
+                }
                 animator.SetBool("Slash", false);
-                animator.SetBool("IsWalking", false);
+                animator.SetBool("IsWalking", true);
             }
         }
     }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        {
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+
 }
-
-
-
-//    else
-//    {
-//        if (agent.remainingDistance <= agent.stoppingDistance) //done with path
-//        {
-//            Vector3 point;
-//            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
-//            {
-//                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
-//                agent.SetDestination(point);
-//            }
-//        }
-//    }
-
-//}
-
-//bool RandomPoint(Vector3 center, float range, out Vector3 result)
-//{
-
-//    Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
-//    NavMeshHit hit;
-//    if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
-//    {
-//        result = hit.position;
-//        return true;
-//    }
-
-//    result = Vector3.zero;
-//    return false;
